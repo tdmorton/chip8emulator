f4d83fc3751d3a258814fbc6e8bd55a49b0c6bd6
Tim Morton <tdmorton22@gmail.com>
2025-03-04 23:17:07 -0500

    starting with just main file, will refactor later

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+#include <cstdint>
 #include <iostream>
+#include <fstream>
+
+class chip8
+{
+	public:
+	uint16_t pc;	// program counter
+	uint16_t sc;	// stack counter
+	uint8_t vreg[16]; // 16 8-bit registers
+	uint16_t stack[16]; // 16 16-bit stack data
+};
+
 
 int main()
 {
 	std::cout << "Hello Worldis" << std::endl;
+	
 	return 0;
 }
